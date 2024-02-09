26111fc3da5850f5c990becb1645c61f84b3ce83
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2024-02-09 10:06:43 +0500

    Create lab3task1.cpp

---
+++ Labs/03/lab3task1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BoardMarker
+{
+    private:
+    string brand;
+    string shade;
+    bool refillable;
+    bool inkstatus;
+
+    public:
+    void set_brand(string brandname)
+    {
+        brand = brandname;
+    }
+    void set_shade(string shadename)
+    {
+        shade = shadename;
+    }
+    void set_refill(bool refillstatus)
+    {
+        refillable = refillstatus;
+    }
+    void set_ink(bool ink)
+    {
+        inkstatus = ink;
+    }
+
+    string get_brand()
+    {
+        return brand;
+    }
+    string get_shade()
+    {
+        return shade;
+    }
+    bool get_refill()
+    {
+        return refillable;
+    }
+    bool set_ink()
+    {
+        return inkstatus;
+    }
+
+    void write()
+    {
+        if (inkstatus)
+        {
+            cout << "Writing with " << brand << " marker in " << shade << " shade.\n";
+        }
+        else
+        {
+            cout << "Ink is depleted. Refill the marker.\n";
+        }
+    }
+
+}
+
+int main()
+{
+
+}
