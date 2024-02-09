1db0c49c86a7113c39b16291de9a09d303573128
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2024-02-09 11:17:27 +0500

    Update lab3task2.cpp

--- Labs/03/lab3task2.cpp
+++ Labs/03/lab3task2.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-#include <string>
-using namespace std;
+/*
+*   Programmer: Ibrahim Johar Farooqi
+*   Date: 9 February 2024
+*/
 
 #include <iostream>
 #include <string>
@@ -8,90 +9,110 @@ using namespace std;
 
 class WaterBottle
 {
-private:
-    string company;
-    string color;
-    double capacityLiters;
-    double capacityMilliliters;
+    private:
+        string company;
+        string color;
+        double capacityLiters;
+        double capacityMilliliters;
 
-public:
-    string getCompany()
-    {
-        return company;
-    }
-
-    string getColor()
-    {
-        return color;
-    }
-
-    double getCapacityLiters()
-    {
-        return capacityLiters;
-    }
-
-    double getCapacityMilliliters()
-    {
-        return capacityMilliliters;
-    }
-
-    void setCompany(string companyName)
-    {
-        company = companyName;
-    }
-
-    void setColor(string bottleColor)
-    {
-        color = bottleColor;
-    }
-
-    void setCapacityMilliliters(double capacityInMilliliters)
-    {
-        capacityMilliliters = capacityInMilliliters;
-        capacityLiters = capacityMilliliters / 1000.0;
-    }
-
-    void setCapacityLiters(double capacityInLiters)
-    {
-        capacityLiters = capacityInLiters;
-        capacityMilliliters = capacityLiters * 1000.0;
-    }
-
-    void updateWaterCapacity(double consumedMilliliters)
-    {
-        if (consumedMilliliters <= capacityMilliliters)
+    public:
+        string get_company()
         {
-            capacityMilliliters -= consumedMilliliters;
+            return company;
+        }
+
+        string get_color()
+        {
+            return color;
+        }
+
+        double get_capacityLiters()
+        {
+            return capacityLiters;
+        }
+
+        double get_capacityMilliliters()
+        {
+            return capacityMilliliters;
+        }
+
+        void set_company(string companyName)
+        {
+            company = companyName;
+        }
+
+        void set_color(string bottleColor)
+        {
+            color = bottleColor;
+        }
+
+        void set_capacityMilliliters(double capacityInMilliliters)
+        {
+            capacityMilliliters = capacityInMilliliters;
             capacityLiters = capacityMilliliters / 1000.0;
         }
-        else
+
+        void set_capacityLiters(double capacityInLiters)
         {
-            cout << "Consumed amount exceeds available water capacity.\n";
+            capacityLiters = capacityInLiters;
+            capacityMilliliters = capacityLiters * 1000.0;
         }
-    }
+
+        void updateWaterCapacity(double consumedMilliliters)
+        {
+            if (consumedMilliliters <= capacityMilliliters)
+            {
+                capacityMilliliters -= consumedMilliliters;
+                capacityLiters = capacityMilliliters / 1000.0;
+            }
+            else
+            {
+                cout << "Consumed amount exceeds available water capacity, cannot update water capacity.\n";
+            }
+        }
 };
 
 
 int main()
 {
+    string company;
+    string color;
+    double capacityLiters;
+    double capacityMilliliters;
+    double water_consumed;
+    
     WaterBottle bottle;
-    bottle.setCompany("ABC Company");
-    bottle.setColor("Blue");
 
-    bottle.setCapacityMilliliters(1500);
-    bottle.setCapacityLiters(1.5);
+    cout << "Enter manufacturer company: ";
+    getline(cin, company);
+    
+    cout << "Enter color: ";
+    getline(cin, color);
 
-    cout << "Water Bottle Information:\n";
-    cout << "Company: " << bottle.getCompany() << "\n";
-    cout << "Color: " << bottle.getColor() << "\n";
-    cout << "Capacity (Liters): " << bottle.getCapacityLiters() << " L\n";
-    cout << "Capacity (Milliliters): " << bottle.getCapacityMilliliters() << " ml\n\n";
+    cout << "Enter capacity in litres: ";
+    cin >> capacityLiters;
 
-    bottle.updateWaterCapacity(300);
+    cout << "Enter capacity in milliliters: ";
+    cin >> capacityMilliliters;
 
-    cout << "After consuming 300 ml of water:\n";
-    cout << "Remaining Capacity (Liters): " << bottle.getCapacityLiters() << " L\n";
-    cout << "Remaining Capacity (Milliliters): " << bottle.getCapacityMilliliters() << " ml\n";
+    bottle.set_company(company);
+    bottle.set_color(color);
+    bottle.set_capacityLiters(capacityLiters);
+    bottle.set_capacityMilliliters(capacityMilliliters);
+
+    cout << "\nWater Bottle Information:\n";
+    cout << "Company: " << bottle.get_company() << "\n";
+    cout << "Color: " << bottle.get_color() << "\n";
+    cout << "Capacity (Liters): " << bottle.get_capacityLiters() << " L\n";
+    cout << "Capacity (Milliliters): " << bottle.get_capacityMilliliters() << " ml\n\n";
+
+    cout << "Enter amount of water consumed: ";
+    cin >> water_consumed;
+
+    bottle.updateWaterCapacity(water_consumed);
+
+    cout << "\nRemaining Capacity (Liters): " << bottle.get_capacityLiters() << " L\n";
+    cout << "Remaining Capacity (Milliliters): " << bottle.get_capacityMilliliters() << " ml\n";
 
     return 0;
 }
